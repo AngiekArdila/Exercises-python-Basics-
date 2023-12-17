5e4283b84db94ee367db8cfa17260971346a549a
Angie Ardila <152419145+AngiekArdila@users.noreply.github.com>
2023-12-16 21:02:17 -0500

    Add files via upload

---
+++ hora_futura.py
@@ -0,0 +1,4 @@
+horaactual=float(input("ingresa la hora actual"))
+enterodehoras=float(input("ingresa le numero de horas"))
+sumatporhoras=(horaactual+enterodehoras)%24
+print("este es el numero de horas"+str(sumatporhoras))
---
+++ pitagoras.py
@@ -0,0 +1,4 @@
+catetoa=float(input("ingrese el valor de a: "))
+catetob=float (input("ingrese el valor de b: "))
+hipotenusa=float(catetoa**2+catetob**2)**0.5
+print("este es el valor de c al cuadrado "+str(hipotenusa))
