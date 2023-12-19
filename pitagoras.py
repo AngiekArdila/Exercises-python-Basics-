514b2497e8b80437f23e81ab744f53c3a416366f
Angie Ardila <152419145+AngiekArdila@users.noreply.github.com>
2023-12-19 08:32:19 -0500

    Delete pitagoras.py

--- pitagoras.py
+++
@@ -1,4 +0,0 @@
-catetoa=float(input("ingrese el valor de a: "))
-catetob=float (input("ingrese el valor de b: "))
-hipotenusa=float(catetoa**2+catetob**2)**0.5
-print("este es el valor de c al cuadrado "+str(hipotenusa))
