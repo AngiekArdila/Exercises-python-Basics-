2c75fadcda1370bed0f6f7add5b8f2a09f9d22f3
Angie Ardila <152419145+AngiekArdila@users.noreply.github.com>
2023-12-19 08:31:57 -0500

    Delete #programa que calcule el promedio eje final .py

--- #programa que calcule el promedio eje final .py
+++
@@ -1,9 +0,0 @@
-#programa que calcule el promedio
-primeranota=int(input("ingresa la primer nota"))
-segundanota=int(input("ingresa la segunda nota"))
-terceranota=int(input("ingresa la tercera nota"))
-cuartanota=int(input("ingresa la cuerta nota"))
-
-promedioes=(primeranota+segundanota+terceranota+cuartanota)/4
-print ("promedio es "+str(promedioes))
-      
