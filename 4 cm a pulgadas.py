a547801c322150ed6d531f26b02c6629a5b36de7
Angie Ardila <152419145+AngiekArdila@users.noreply.github.com>
2023-12-19 08:34:43 -0500

    Add files via upload

---
+++ 4 cm a pulgadas.py
@@ -0,0 +1,3 @@
+longitud=int(input("ingrese la longitud en cm"))
+convertidor=longitud/2.54
+print("este es el valor en pulgadas"+str(convertidor))
---
+++ 7. parte decimal.py
@@ -0,0 +1,3 @@
+numeroreal=float(input("Ingresa el numero real: "))
+partedecimal="{:.2f}".format (numeroreal-int(numeroreal))
+print("Esta es la parte decimal: "+str(partedecimal))
