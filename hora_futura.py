d4b048464795163ea89a424701ba91ba4bba4439
Angie Ardila <152419145+AngiekArdila@users.noreply.github.com>
2023-12-19 08:32:09 -0500

    Delete hora_futura.py

--- hora_futura.py
+++
@@ -1,4 +0,0 @@
-horaactual=float(input("ingresa la hora actual"))
-enterodehoras=float(input("ingresa le numero de horas"))
-sumatporhoras=(horaactual+enterodehoras)%24
-print("este es el numero de horas"+str(sumatporhoras))
